a158cadb35e88c9eed002cd940d0a3219fe37518
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-05-19 15:24:48 -0500

    hospedajes

--- src/App/App.java
+++ src/App/App.java
@@ -5,22 +5,221 @@ public class App {
     public static void main(String[] args) {
 
         // creates one million sql sentences for the creation of information
-        for (int i = 1; i <= 1000000; i++) {
-            if (i <= 100000 / 3) {
-                operadores(i);
+        int sentences = 0;
+        int idCounter = 1;
+
+        while (sentences < 1000000) {
+            // each operator creates 3 sql sentences
+            if (sentences <= 100000) {
+                operadores(idCounter);
+                sentences += 3;
+            } else if (sentences <= 200000) {
+                // each alojamiento creates 2 sql sentences
+                alojamientos(idCounter);
+                sentences += 2;
+            } else if (sentences <= 400000) {
+                // each hospedaje creates up to 3 sql sentences
+                hospedajes(idCounter);
+                sentences += 4;
+            } else {
+                break;
             }
-            if (i > 100000 / 3 && i <= 100000 / 3 * 2) {
-                alojamientos(i);
-            }
+            idCounter++;
         }
+
+    }
+
+    public static void vecino(int id) {
+        // creates a sql sentence for the creation of a Vecino
+        // INSERT INTO A_Vecino (Nombre, DocumentoDeIdentidad, Datos) VALUES
+        // ('Juanito el vecino', 123123, 7);
+
+        // the name of the vecino can be 'Juanito el vecino', 'Pedrito el vecino',
+        // 'Pablito el vecino', 'Carlitos el vecino', 'Pepito el vecino'
+        String nombre = "";
+        int nombreRandom = (int) Math.floor(Math.random() * (5 - 1 + 1) + 1);
+        if (nombreRandom == 1) {
+            nombre = "Juanito el vecino";
+        } else if (nombreRandom == 2) {
+            nombre = "Pedrito el vecino";
+        } else if (nombreRandom == 3) {
+            nombre = "Pablito el vecino";
+        } else if (nombreRandom == 4) {
+            nombre = "Carlitos el vecino";
+        } else if (nombreRandom == 5) {
+            nombre = "Pepito el vecino";
+        }
+
+        // the documentoDeIdentidad is between 100000 and 1000000
+        int documentoDeIdentidad = (int) Math.floor(Math.random() * (1000000 - 100000 + 1) + 100000);
+
+        // we create a new datos
+        datos(id);
+
+        System.out.println("INSERT INTO A_Vecino (Nombre, DocumentoDeIdentidad, Datos) VALUES");
+        System.out.println("('" + nombre + "', " + documentoDeIdentidad + ", " + id + ");");
+
+    }
+
+    public static void apartamento(int id) {
+        // creates a sql sentence for the creation of a Apartamento
+        // example: INSERT INTO A_Apartamento (Id, NumHabitaciones, Menaje, CostoTotal,
+        // CostoArrendamiento, CostoInmueble, VecinoDueno) VALUES
+        // (5, 5, 'colchon', 500000, 340000, 23000, 123123);
+
+        // we first create a vecino
+        vecino(id);
+
+        // the number of habitaciones is between 1 and 10
+        int numHabitaciones = (int) Math.floor(Math.random() * (10 - 1 + 1) + 1);
+
+        // the menaje can be 'colchon', 'sabanas', 'almohada', 'televisor', 'nevera',
+        // 'lavadora', 'secadora', 'estufa', 'horno', 'microondas', 'licuadora',
+        String menaje = "";
+        int menajeRandom = (int) Math.floor(Math.random() * (11 - 1 + 1) + 1);
+        if (menajeRandom == 1) {
+            menaje = "colchon";
+        } else if (menajeRandom == 2) {
+            menaje = "sabanas";
+        } else if (menajeRandom == 3) {
+            menaje = "almohada";
+        } else if (menajeRandom == 4) {
+            menaje = "televisor";
+        } else if (menajeRandom == 5) {
+            menaje = "nevera";
+        } else if (menajeRandom == 6) {
+            menaje = "lavadora";
+        } else if (menajeRandom == 7) {
+            menaje = "secadora";
+        } else if (menajeRandom == 8) {
+            menaje = "estufa";
+        } else if (menajeRandom == 9) {
+            menaje = "horno";
+        } else if (menajeRandom == 10) {
+            menaje = "microondas";
+        } else if (menajeRandom == 11) {
+            menaje = "licuadora";
+        }
+
+        // the costoTotal is between 100000 and 1000000
+        int costoTotal = (int) Math.floor(Math.random() * (1000000 - 100000 + 1) + 100000);
+
+        // the costoArrendamiento is between 100000 and 1000000
+        int costoArrendamiento = (int) Math.floor(Math.random() * (1000000 - 100000 + 1) + 100000);
+
+        // the costoInmueble is between 10000 and 100000
+        int costoInmueble = (int) Math.floor(Math.random() * (100000 - 10000 + 1) + 10000);
+
+        System.out.println(
+                "INSERT INTO A_Apartamento (Id, NumHabitaciones, Menaje, CostoTotal, CostoArrendamiento, CostoInmueble, VecinoDueno) VALUES");
+        System.out.println("(" + id + ", " + numHabitaciones + ", '" + menaje + "', " + costoTotal + ", "
+                + costoArrendamiento + ", " + costoInmueble + ", " + id + ");");
+
+    }
+
+    public static void habitacion(int id) {
+        // creates a sql sentence for the creation of a Habitacion
+        // example: INSERT INTO A_Habitacion (Id, Costo, TipoDeHabitacion) VALUES
+        // (1, 30000, 'Individual');
+
+        // the cost of the habitacion is between 10000 and 50000
+        int costo = (int) Math.floor(Math.random() * (50000 - 10000 + 1) + 10000);
+
+        // the type of the habitacion can be 'Estandard', 'SemiDeluxe', 'Deluxe',
+        // 'Individual', 'Compartido'
+        String tipoDeHabitacion = "";
+        int tipoDeHabitacionRandom = (int) Math.floor(Math.random() * (5 - 1 + 1) + 1);
+        if (tipoDeHabitacionRandom == 1) {
+            tipoDeHabitacion = "Estandard";
+        } else if (tipoDeHabitacionRandom == 2) {
+            tipoDeHabitacion = "SemiDeluxe";
+        } else if (tipoDeHabitacionRandom == 3) {
+            tipoDeHabitacion = "Deluxe";
+        } else if (tipoDeHabitacionRandom == 4) {
+            tipoDeHabitacion = "Individual";
+        } else if (tipoDeHabitacionRandom == 5) {
+            tipoDeHabitacion = "Compartido";
+        }
+
+        System.out.println("INSERT INTO A_Habitacion (Id, Costo, TipoDeHabitacion) VALUES");
+        System.out.println("(" + id + ", " + costo + ", '" + tipoDeHabitacion + "');");
+
+    }
+
+    public static void hospedajes(int id) {
+        // creates a sql sentence for the creation of a Hospedaje
+        // example: INSERT INTO A_HospedajeAbstract (Id, Metraje, Alojamiento,
+        // FormaDePago, Capacidad) VALUES
+        // (1, 3, 1, 'Diario', 1);
+
+        int metraje = (int) Math.floor(Math.random() * (100 - 1 + 1) + 1);
+
+        // alojamientos are created from 100001 to 200000
+        int alojamiento = (int) Math.floor(Math.random() * (200000 - 100001 + 1) + 100001);
+
+        // formaDePago can be Diario, Mensual, Semestral
+        String formaDePago = "";
+        int formaDePagoRandom = (int) Math.floor(Math.random() * (3 - 1 + 1) + 1);
+        if (formaDePagoRandom == 1) {
+            formaDePago = "Diario";
+        } else if (formaDePagoRandom == 2) {
+            formaDePago = "Mensual";
+        } else if (formaDePagoRandom == 3) {
+            formaDePago = "Semestral";
+        }
+        int capacidad = (int) Math.floor(Math.random() * (100 - 1 + 1) + 1);
+
+        System.out.println(
+                "INSERT INTO A_HospedajeAbstract (Id, Metraje, Alojamiento, FormaDePago, Capacidad) VALUES");
+        System.out.println("(" + id + ", " + metraje + ", " + alojamiento + ", '" + formaDePago + "', " + capacidad
+                + ");");
+
+        // decides if the hospedaje is a habitacion or a apartamento
+        if (Math.random() < 0.5) {
+            habitacion(id);
+        } else {
+            apartamento(id);
+        }
+
+    }
+
+    public static void hotel(int id) {
+        // creates a sql sentence for the creation of a Hotel
+        // example: INSERT INTO A_Hotel (Id, NumEstrellas) VALUES
+        // (3, 4);
+
+        int numEstrellas = (int) Math.floor(Math.random() * (5 - 1 + 1) + 1);
+
+        System.out.println("INSERT INTO A_Hotel (Id, NumEstrellas) VALUES");
+        System.out.println("(" + id + ", " + numEstrellas + ");");
+
+    }
+
+    public static void hostal(int id) {
+        // creates a sql sentence for the creation of a Hostal
+        // example: INSERT INTO A_Hostal (Id, HorarioDeApertura, HorarioDeCierre,
+        // NumEstrellas) VALUES
+        // (1, 900, 1900, 2);
+
+        // opens in the morning between 6 and 10
+        int horarioDeApertura = (int) Math.floor(Math.random() * (1000 - 600 + 1) + 600);
+
+        // closes in the afternoon between 18 and 22
+        int horarioDeCierre = (int) Math.floor(Math.random() * (2200 - 1800 + 1) + 1800);
+
+        // has between 1 and 3 stars
+        int numEstrellas = (int) Math.floor(Math.random() * (3 - 1 + 1) + 1);
+
+        System.out.println("INSERT INTO A_Hostal (Id, HorarioDeApertura, HorarioDeCierre, NumEstrellas) VALUES");
+        System.out.println("(" + id + ", " + horarioDeApertura + ", " + horarioDeCierre + ", " + numEstrellas + ");");
 
     }
 
     public static void alojamientos(int id) {
         // creates a sql sentence for the creation of an Alojamiento
         // INSERT INTO A_AlojamientoAbstract (Id, Nombre, Direccion, Capacidad, Ciudad,
-        // Operador, Fechainiciodisponibilidad, Duraciondisponibilidad) VALUES
-        // (1, 'Hostal 1', 'Direccion1', 10, 'Ciudad1', 1, DATE '2023-05-06', 100);
+        // Operador, Fechainiciodisponibilidad, Duraciondisponibilidad, Activo) VALUES
+        // (1, 'Hostal 1', 'Direccion1', 10, 'Ciudad1', 1, DATE '2023-05-06', 100, 1);
 
         String nombre = "alojamiento" + id;
         String direccion = "direccion" + id;
@@ -37,9 +236,15 @@ public class App {
         System.out.println(
                 "INSERT INTO A_AlojamientoAbstract (Id, Nombre, Direccion, Capacidad, Ciudad, Operador, Fechainiciodisponibilidad, Duraciondisponibilidad) VALUES");
         System.out.println("(" + id + ", '" + nombre + "', '" + direccion + "', " + capacidad + ", '" + ciudad + "', "
-                + operador + ", DATE '" + fechaInicioDisponibilidad + "', " + duracionDisponibilidad + ");");
+                + operador + ", DATE '" + fechaInicioDisponibilidad + "', " + duracionDisponibilidad + "', " + 1
+                + ");");
 
         // depending on the id, it will create a hotel of hostal
+        if (Math.random() < 0.5) {
+            hotel(id);
+        } else {
+            hostal(id);
+        }
 
     }
 
@@ -94,7 +299,7 @@ public class App {
         // depending on the id, it will create a sentence
         // for the creation of a PersonaNatural or a Empresa
 
-        if (id % 2 == 0) {
+        if (Math.random() < 0.5) {
             personaNatural(id);
         } else {
             empresa(id);
